1b6bd93ecca8c8ba91f5dfe5bfbd6fb360b7af06
Zhidong Zhang <zzhidong@amazon.com>
2023-05-25 13:02:42 -0700

    add argparse

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import string
 from jinja2 import Template
 import smtplib
 from email.mime.text import MIMEText
+import argparse
 
 def column_index_to_code(column_index):
     column_code_chars = []
@@ -24,10 +25,18 @@ def send_email(subject, body, sender, recipients, cc_recipients, password):
     smtp_server.quit()
 
 def main():
-    with open('template.html', 'r') as f:
+    parser = argparse.ArgumentParser(description='PerfReviewEmailSender')
+    
+    parser.add_argument("-e", "--email", help="Sender email", default="zhidongzhang.work@gmail.com")
+    parser.add_argument("-p", "--password", help="Sender email password", default="vgnnntyzmdnrepvq")
+    parser.add_argument("-d", "--data", help="Employee performance data in csv", default="sample_data.csv")
+    parser.add_argument("-t", "--template", help="An email template in html", default="template.html")
+    args = parser.parse_args()
+
+    with open(args.template, 'r') as f:
         template = Template(f.read())
 
-    df = pd.read_csv('sample_data.csv')
+    df = pd.read_csv(args.data)
 
     htmls = []
     for _, row in df.iterrows():
@@ -40,10 +49,10 @@ def main():
         
         subject = "Performance Review for {}".format(context["D"])
         body = html
-        sender = "zhidongzhang.work@gmail.com"
+        sender = args.email
         recipients = context["B"].split(",")
         cc_recipients = context["C"].split(",")
-        password = "vgnnntyzmdnrepvq"
+        password = args.password
         send_email(subject, body, sender, recipients, cc_recipients, password)
 
 if __name__ == "__main__":
