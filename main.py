006caf0536f32a679c34561f6d7fa6a2202650e8
Zhidong Zhang <zzhidong@amazon.com>
2023-05-25 23:59:08 -0700

    add CC table

--- main.py
+++ main.py
@@ -26,12 +26,23 @@ def send_email(subject, body, sender, recipients, cc_recipients, password):
 
 def main():
     parser = argparse.ArgumentParser(description='PerfReviewEmailSender')
-    
     parser.add_argument("-e", "--email", help="Sender email", default="zhidongzhang.work@gmail.com")
     parser.add_argument("-p", "--password", help="Sender email password", default="vgnnntyzmdnrepvq")
     parser.add_argument("-d", "--data", help="Employee performance data in csv", default="sample_data.csv")
     parser.add_argument("-t", "--template", help="An email template in html", default="template.html")
+    parser.add_argument("-c", "--cc", help="CC Lists in csv", default="cc.csv")
+    parser.add_argument("-y", "--year", help="Performance Review Year", default="2023")
+    parser.add_argument("-m", "--month", help="Performance Review Month", default="Apirl")
+    parser.add_argument("-n", "--name_column", help="The column code for name column", default="D")
     args = parser.parse_args()
+    
+    # Build CC Map
+    cc_df = pd.read_csv(args.cc)
+    name_to_recipients = {}
+    name_to_cc_recipients = {}
+    for _, row in cc_df.iterrows():
+        name_to_recipients[row[0]] = row[1].split(",")
+        name_to_cc_recipients[row[0]] = row[2].split(",")    
 
     with open(args.template, 'r') as f:
         template = Template(f.read())
@@ -44,14 +55,15 @@ def main():
         for column_index, item in enumerate(row):
             column_code = column_index_to_code(column_index) 
             context[column_code] = item
+        context["month"] = args.month
         html = template.render(context)
         htmls.append(html)
         
-        subject = "Performance Review for {}".format(context["D"])
+        subject = "{} {} Statistics Task Review".format(args.year, args.month)
         body = html
         sender = args.email
-        recipients = context["B"].split(",")
-        cc_recipients = context["C"].split(",")
+        recipients = name_to_recipients[context[args.name_column]]
+        cc_recipients = name_to_cc_recipients[context[args.name_column]]
         password = args.password
         send_email(subject, body, sender, recipients, cc_recipients, password)
 
